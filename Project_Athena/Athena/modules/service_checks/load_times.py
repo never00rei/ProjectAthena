75440e528abbcd18c4339941eae76d6dd4ec1532
Never00Rei <jwashington104.jw@gmail.com>
2018-01-20 10:22:34 +0000

    Updated stuff

--- Project_Athena/Athena/modules/service_checks/load_times.py
+++ Project_Athena/Athena/modules/service_checks/load_times.py
@@ -5,10 +5,14 @@ def check_load_time(url):
     Requests the url of a service and returns the total time taken
     to render the url.
     :param url:
-    :return:
+    :return: dictionary of items...
     """
-    if url == None:
-        return "Empty URL"
+
+    if url is None:
+        return {'status': '404'}
     else:
-        time_elapsed = requests.get(url).elapsed.total_seconds()
-        return time_elapsed
+        try:
+            time_elapsed = requests.get(url).elapsed.total_seconds()
+            return {'status': '200', 'response_time': time_elapsed}
+        except:
+            return {'status': '503'}
--- Project_Athena/Athena/modules/services.py
+++ Project_Athena/Athena/modules/services.py
@@ -45,7 +45,7 @@ def choose_replies(init_resps=None, std_resps=None, err_resps=None, missing_resp
 
 def gen_keyword_regex():
     """
-
+    Scans services and builds a list of keywords for use in searches.
     :return:
     """
     services = postgres.db_session.query(Service).all()
@@ -58,7 +58,9 @@ def gen_keyword_regex():
     return keywords
 
 
-@listen_to('What services do we have|List services', re.IGNORECASE)
+@listen_to('list all services', re.IGNORECASE)
+@listen_to('list services', re.IGNORECASE)
+@listen_to('What services do we have', re.IGNORECASE)
 def channel_service_message(message):
     """
     Scans channel for messages asking about what services currently have integration.
@@ -83,8 +85,9 @@ def channel_service_message(message):
                          json.dumps(attachments),
                          in_thread=True)
 
-
-@respond_to('What services do we have|List services', re.IGNORECASE)
+@respond_to('list all services', re.IGNORECASE)
+@respond_to('list services', re.IGNORECASE)
+@respond_to('What services do we have', re.IGNORECASE)
 def direct_service_message(message):
     """
     Listens for direct messages asking about what services currently have integration.
@@ -108,6 +111,7 @@ def direct_service_message(message):
                          json.dumps(attachments))
 
 
+@listen_to('Is (.*) going slow', re.IGNORECASE)
 @listen_to('Is (.*) running slow', re.IGNORECASE)
 def service_check(message, service):
     init_replies = ["I'll check on that for you. :smiley:", "Let's have a look! :smiley:"]
@@ -122,7 +126,17 @@ def service_check(message, service):
     else:
         staged = postgres.db_session.query(ServiceCheck).filter(ServiceCheck.service_id == query[0].id).all()
         if staged is not None:
-            load_time = check_load_time(staged[0].check_url)
-            message.reply(std_reply.format(query[0].service_name, load_time))
+            response = check_load_time(staged[0].check_url)
+            print(response)
+            if response is not None:
+                message.reply(std_reply.format(query[0].service_name, response['response_time']))
+            elif response['status'] == '503':
+                message.reply(err_reply)
+            elif response['status'] == '404':
+                message.reply(missing_reply)
+            else:
+                message.reply(err_reply)
         else:
             message.reply(err_reply)
+
+
