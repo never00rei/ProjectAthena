b32cd076fa30877eb4923fe7f16e7b4c38daf22c
james <jwashington104.jw@gmail.com>
2018-05-30 22:12:41 +0100

    Refactored db_setup.py to have a cleaner db_url variable.

--- Project_Athena/Athena/modules/db_adapter/db_setup.py
+++ Project_Athena/Athena/modules/db_adapter/db_setup.py
@@ -6,9 +6,7 @@ def setup():
     PGPORT = getenv('PGPORT', 5433)
     PGPASS = getenv('PGPASS', 'athena')
     PGDATABASE = getenv('PGDATABASE', 'application')
-    db_url= 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(PGUSER,
-                                                      PGPASS,
-                                                      PGHOST,
-                                                      PGPORT,
-                                                      PGDATABASE)
+
+    db_url = f'postgresql://{PGUSER}:{PGPASS}@{PGHOST}:{PGPORT}/{PGDATABASE}'
+
     return db_url
